7be77c942078b80f37c3fa2e3ae3749a21a14b96
Ella Wirstad <ella.wg@hotmail.com>
2019-03-14 15:14:35 +0100

    add rolling text

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/start">Start</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  mounted() {
+    this.starGenerator();
+  },
+  methods: {
+    starGenerator() {
+      // https://dev.to/christopherkade/developing-the-star-wars-opening-crawl-in-htmlcss-2j9e
+      // Sets the amount of stars to generate
+      const starAmount = 100;
+
+      // Function that generates random x, y values based on container size
+      function getRandomPosition() {
+        var y = window.innerWidth;
+        var x = window.innerHeight;
+        var randomX = Math.floor(Math.random() * x);
+        var randomY = Math.floor(Math.random() * y);
+        return [randomX, randomY];
+      }
+
+      for (let i = 0; i < starAmount; i++) {
+        let star = document.createElement("div");
+        star.className = "star";
+        var xy = getRandomPosition();
+        star.style.top = xy[0] + "px";
+        star.style.left = xy[1] + "px";
+        document.body.append(star);
+      }
+    }
+  }
+};
+</script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
-body{
-    background-color:#1e2326;
+@import url("https://fonts.googleapis.com/css?family=Roboto+Mono");
+body {
+  background-color: #1e2326;
+}
 
+.star {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  background-color: white;
+  z-index: -100;
 }
 
 #app {
-  font-family: 'Roboto Mono', monospace;
+  font-family: "Roboto Mono", monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #ffffff;
   margin-top: 60px;
-
 }
 </style>
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <button class="evacuate-button">EVACUATE ME</button>
+    <!-- Change the text to your liking -->
+    <div class="board">
+      <div class="content">
+        <p class="title">Episode I</p>
+        <p class="subtitle">THE EVACUATION</p>
+        <br>
+        <p>Turmoil has engulfed the Republic of Earth as the sun no longer shines.</p>
+        <p>Every human is being evacuated to a new planet.</p>
+        <p>This is your evacuation guide to the galaxy.</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +24,55 @@ export default {
 };
 </script>
 <style scoped>
+p {
+  color: #FFFF82;
+}
+
+/* Set the font, lean the board, position it */
+.board {
+  transform: perspective(300px) rotateX(25deg);
+  transform-origin: 50% 100%;
+  text-align: justify;
+  position: absolute;
+  margin-left: -9em;
+  font-weight: bold;
+  overflow: hidden;
+  font-size: 350%;
+  height: 50em;
+  width: 18em;
+  bottom: 0;
+  left: 50%;
+}
+
+.board:after {
+  position: absolute;
+  content: ' ';
+  bottom: 60%;
+  left: 0;
+  right: 0;
+  top: 0;
+}
+
+/* Set the scrolling animation and position it */
+.content {
+  animation: scroll 110s linear 1s;
+  position: absolute;
+  top: 100%;
+}
+
+.title, .subtitle {
+  text-align: center;
+}
+
+@keyframes scroll {
+    0% {
+        top: 100%;
+    }
+    100% {
+        top: -170%;
+    }
+}
+
 .evacuate-button {
   font-family: "Roboto Mono", monospace;
   font-size: 2em;
@@ -26,13 +86,15 @@ export default {
   overflow: hidden;
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.29);
   border-bottom: solid 5px #bd6565;
-  transition: .4s;
+  transition: 0.4s;
 }
 
-.evacuate-button:hover {background-color: #94250c}
+.evacuate-button:hover {
+  background-color: #94250c;
+}
 
 .evacuate-button:active {
-  background-color:#94250c;
+  background-color: #94250c;
   -ms-transform: translateY(2px);
   -webkit-transform: translateY(2px);
   transform: translateY(2px);
@@ -40,6 +102,7 @@ export default {
   border-bottom: none;
 }
 
-.evacuate-button:focus { outline: none; }
-
+.evacuate-button:focus {
+  outline: none;
+}
 </style>
