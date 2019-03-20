f1eaac9799375f84145507c8176e86254d525d59
Ella Wirstad <ella.wg@hotmail.com>
2019-03-20 21:06:49 +0100

    get and list climates from swapi

--- src/views/Climate.vue
+++ src/views/Climate.vue
@@ -2,6 +2,9 @@
 <template>
   <div>
     <div class="infotext">Välkommen. Här väljs klimat...</div>
+    <ul class="climate-list">
+      <li v-for="climate in $store.getters.planetClimates" :key="climate">{{climate}}</li>
+    </ul>
     <v-btn class="nextButton" @click="$router.push('/population')">Next</v-btn>
   </div>
 </template>
@@ -9,9 +12,10 @@
 <script>
 export default {
   name: "Climate",
-  mounted: function() {
+  mounted() {
     this.$store.state.currentStep = 1;
-  }
+    this.$store.dispatch('fetchSwapi');
+  },
 };
 </script>
 
