94ef277fe73b781dfa13e088c729aa6e260232fb
Johannes <johannes@n148-p66.eduroam.kth.se>
2019-03-21 16:57:27 +0100

    Created the basics for the time page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCircleSlider from 'vue-circle-slider'
 
+Vue.use(VueCircleSlider)
 Vue.config.productionTip = false
 
 new Vue({
