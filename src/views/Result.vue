2892d9b47e26e422737cece49a5bf30a50feaba8
lindacnatt <linda.cnattingius@gmail.com>
2019-04-16 17:45:02 +0200

    add animation to result page and Time page

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <div class="infotext">Här visas resultatet...</div>
+  <div class="results">
+    <div class="info-text">Här visas resultatet...</div>
+    <div class="box-fluid">
+       <div class="animation">
+        <div class="planet" style="z-index: 1"></div>
+        <img class="rocket" src="../assets/rocket.png" style="z-index: 0;">
+      </div>
+      <div class="planet-result">
+        <p>You will be evacuated to...</p>
+        <p class="name-of-planet">PLUTO</p>
+        <p class="population">population</p>
+        <p class="Gravity">Gravity</p>
+        <p class="Time">Time</p>
+      </div>
+     
+    </div>
   </div>
 </template>
 
@@ -15,4 +29,61 @@ export default {
 
 
 <style scoped>
+.box-fluid{
+  display:flex;
+  flex-flow: row wrap;
+  justify-content:center;
+  margin-top: 50px;
+
+}
+.animation {
+  display: flex;
+  justify-content: flex-start;
+  max-width:900px;
+  min-width:500px;
+  max-height: 300px;
+  min-height: 300px;
+  position: relative;
+  left:0px;
+}
+.planet {
+  position: absolute;
+  width: 120px;
+  height: 120px;
+  background: blue;
+  border-radius: 50%;
+  left: 200px;
+  top: 90px;
+  box-shadow: 0 0 30px #3a72d3;
+}
+.rocket {
+  position: absolute;
+  width: 50px;
+  left: 240px;
+  top: 80px;
+  -webkit-animation: myOrbit 4s linear forwards; /* Chrome, Safari 5 */
+}
+.planet-result{
+  display:flex;
+  flex-direction: column;
+  max-width:900px;
+  min-width:500px;
+
+}
+
+@keyframes myOrbit {
+  0% {
+    -webkit-transform: rotate(180deg) translateX(150px);
+  }
+  70% {
+    -webkit-transform: rotate(-135deg) translateX(150px);
+    top: 80px;
+    left: 240px;
+  }
+  100% {
+    -webkit-transform: rotate(-135deg) translateX(150px) rotate(97deg);
+    top: 125px;
+    left: 280px;
+  }
+}
 </style>
